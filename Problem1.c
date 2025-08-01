3982cba14bf892f6e644ca7ec2c28c7ac478a7f4
Aminur131 <aminurrahmanmahin74@gmail.com>
2025-08-01 20:04:30 +0600

    Update Problem1.c

--- Problem1.c
+++ Problem1.c
@@ -1,1 +1,27 @@
-eikhane code dibe
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   double lamda,d,theta;
+   double theta_rad,m;
+
+   printf("Enter Wavelength (nanometers):");
+   scanf("%lf",&lamda);
+
+   printf("Enter Slit distance d(micrometers):");
+   scanf("%lf",&d);
+
+    printf("Enter Angle theta (degrees):");
+    scanf("%lf",&theta);
+
+    theta_rad = theta*3.1416/180;
+
+    m=(d*0.000001*sin(theta*3.1416/180))/(lamda*0.000000001);
+
+    printf("Order of maxima(m)= %.2lf\n",m);
+
+    getchar();
+}
+
+
+
