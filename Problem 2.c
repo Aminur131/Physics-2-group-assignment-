d3165d1468e1f88dd6266399ff972c330cd63cf9
Aminur131 <aminurrahmanmahin74@gmail.com>
2025-08-01 20:36:52 +0600

    Update Problem 2.c

--- Problem 2.c
+++ Problem 2.c
@@ -1,1 +1,17 @@
-eikhane shot dibe
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   double ratio;
+   double angle=45;
+   double pi = 3.1416;
+
+   ratio = 1/sin(angle*pi/180);
+
+   printf("The Ratio (a/lamda) is :%.2f\n",ratio);
+
+   getchar();
+
+
+
+}
